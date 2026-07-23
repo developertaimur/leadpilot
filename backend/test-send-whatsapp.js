2488ff43ea837b270849ed1771d8ffc6f6497bbb
developertaimur <muhammadtaimur2001@gmail.com>
2026-07-24 04:19:47 +0500

    Remove temporary WhatsApp test scripts

--- backend/test-send-whatsapp.js
+++
@@ -1,8 +0,0 @@
-const { sendWhatsAppMessage } = require('./src/infrastructure/whatsapp/whatsapp.client');
-
-async function run() {
-  const result = await sendWhatsAppMessage('923475029433', 'Hello from gymfinity!');
-  console.log(result);
-}
-
-run();
--- backend/test-whatsapp-config.js
+++
@@ -1,7 +0,0 @@
-const env = require('./src/config/env');
-console.log({
-  hasToken: !!env.whatsappAccessToken,
-  phoneNumberId: env.whatsappPhoneNumberId,
-  wabaId: env.whatsappBusinessAccountId,
-  hasVerifyToken: !!env.whatsappWebhookVerifyToken,
-});
